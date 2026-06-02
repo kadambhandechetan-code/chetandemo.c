202c859653725fbf2888f2b44b08403ee06ce148
kadambhandechetan-code <kadambhandechetan@gmail.com>
2026-06-02 15:33:42 +0530

    Update .chetan.c

--- .chetan.c
+++ .chetan.c
@@ -1,11 +1,6 @@
-#include<stdio.h>
-int main()
-{
-    for (int i = 0; i <=100; i++)
-    {
-        printf("%d i love you \n",i);
+#include <stdio.h>
 
-    }
+int main() {
+    printf("Hello, GitHub!\n");
     return 0;
-
 }
