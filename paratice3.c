0943a2cf854d5a789fd77db401d4a6c22a40df7a
kadambhandechetan-code <kadambhandechetan@gmail.com>
2026-06-08 15:07:52 +0530

    Add files via upload

---
+++ paratice3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+   int main(){
+   	
+   	 int celsius,temperature,fahrenheit;
+   	 
+   	 printf("Enter a temperature,celsius ");
+   	 scanf("%f", &fahrenheit);
+   	 
+   	 fahrenheit=(9 *celsius)/ 5 + 32;
+   	 
+   	 printf("temperature fahrenheit=%d", fahrenheit);
+   	 
+   	 return 0;
+   }
