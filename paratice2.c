8bf97c8b96e7f677efa34e416e9719382acdca7f
kadambhandechetan-code <kadambhandechetan@gmail.com>
2026-06-08 15:04:52 +0530

    Add files via upload

---
+++ paratice2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+  int main(){
+  	 int num ,square,cube;
+  	 
+  	 printf("Enter a num");
+  	 scanf("%d",&num);
+  	 
+  	 square= num + num;
+  	 cube= num + num + num;
+  	 
+  	 printf("square %d",square);
+  	 printf("cube %d", cube);
+  	 
+  	 return 0;
+  	 
+  }
