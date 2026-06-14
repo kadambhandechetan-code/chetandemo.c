957f0d6ad09cbaaad71bc7c7ea33bfbdad7f0d02
kadambhandechetan-code <kadambhandechetan@gmail.com>
2026-06-14 14:23:00 +0530

    c Assigment01
    write a program to check whether a number is even or odd

---
+++ Paratice01.cpp
@@ -0,0 +1,22 @@
+//Assignment 01 
+// write a program to check whether a number is even of odd
+
+#include<stdio.h>
+int main()
+{
+
+   int num;
+   
+    printf("Enter number :");
+    scanf("%d",&num);
+    
+   if(num %2 == 0 )
+    {
+    	printf("the number is even");
+	}
+   else 
+	{
+		printf("the number is odd");
+	}
+	    return 0;
+ }
