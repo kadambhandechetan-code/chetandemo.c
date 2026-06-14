d8c1b007b8ddf9248135854519339eb942ad09d9
kadambhandechetan-code <kadambhandechetan@gmail.com>
2026-06-14 14:25:01 +0530

    Add files via upload

---
+++ paratice03.c
@@ -0,0 +1,23 @@
+//Aassignement 01 
+//write a program to chcek whether a given year is a leap year 
+
+#include<stdio.h>
+
+int main()
+{
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+    {
+        printf("%d is a Leap Year", year);
+    }
+    else
+    {
+        printf("%d is Not a Leap Year", year);
+    }
+
+    return 0;
+}
