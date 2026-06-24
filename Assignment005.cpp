1df7fd169af3b741ea87d7c4c9a356757bb18a2e
kadambhandechetan-code <kadambhandechetan@gmail.com>
2026-06-24 13:17:20 +0530

    Assignment.c
    Accept the price from user. Ask the user if he is a student (user may say y or n). If he
    is a student and he has purchased more than 500 than discount is 20% otherwise
    discount is 10%.But if he is not a student then if he has purchased more than 600
    discount is 15% otherwise there is not discount.

---
+++ Assignment005.cpp
@@ -0,0 +1,51 @@
+//Assignment01
+//Accept the price from user. Ask the user if he is a student (user may say y or n). If he
+//is a student and he has purchased more than 500 than discount is 20% otherwise
+//discount is 10%.But if he is not a student then if he has purchased more than 600
+//discount is 15% otherwise there is not discount.
+
+#include<stdio.h>
+int main()
+{
+char student;
+int price;
+
+   printf("Enter student y/n: ");
+   scanf("%c", &student);
+   
+   printf("Enter price: ");
+   scanf("%d", &price);
+
+if(student == 'y')
+{
+    
+
+    if(price > 500)
+    {
+        printf("20%% discount");
+    }
+    else
+    {
+        printf("10%% discount");
+    }
+}
+else
+{
+	if (student=='n')
+    {
+    
+    
+    if(price > 600)
+    {
+        printf("15%% discount");
+    }
+    else
+    {
+       printf("no discount");
+    }
+     
+       return 0;
+       
+    }
+  }
+}
