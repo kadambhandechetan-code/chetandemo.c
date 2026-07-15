ea274c9a66e4a0c69cd7a70885735f4d73cd9631
kadambhandechetan-code <kadambhandechetan@gmail.com>
2026-07-15 22:53:51 +0530

    Add files via upload

---
+++ loops1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,flag=i,prime;
+	
+	printf("Enter number");
+	scanf("%d", &n);
+  
+   for(i=0; i<=prime; i++ )	
+	{
+		flag = flag = i;
+	}
+	if(n=20)
+	{
+		printf("prime number");
+	}
+	
+	    return 0;
+}
