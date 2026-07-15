392b6c67539e52a90afc38d18a693698d778d319
kadambhandechetan-code <kadambhandechetan@gmail.com>
2026-07-15 22:49:01 +0530

    Add files via upload

---
+++ forloops3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+   int main()
+{
+	int n,i;
+	int prime;
+	printf("Enter number:");
+	scanf("%d", &n);
+	
+digit = temp % 10;
+	{
+	
+		printf("prime=%d\n",prime);
+		
+		printf("not a prime number");
+	}
+	    
+
+	  
+	  return 0;
+}
