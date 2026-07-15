35edd98f39185718d1a02f68327c7e74dbce2313
kadambhandechetan-code <kadambhandechetan@gmail.com>
2026-07-15 22:50:09 +0530

    Add files via upload

---
+++ forloops5.c
@@ -0,0 +1,31 @@
+//Assignment 
+//Check the given number is Armstrong number or not.. Input: n = 153 Output: Armstrong // loop use
+
+#include<stdio.h>
+int main()
+{
+	int n,r,sum=0,temp;
+	printf("Enter number");
+	scanf("%d", &n);
+	
+	temp=n;
+	
+   while(n<0)
+	{
+		r=n%10;
+		sum=sum+(r*r*r);
+		n=n/10;
+	}
+	if(temp==sum)
+	{
+		printf("Armstrong number");
+	}
+	else
+	{
+		printf("not Armstrong number");
+	}
+	
+	    
+	     return 0;
+	
+}
