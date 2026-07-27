5b84805373af5397d3afc706e9db12ebf9169be3
kadambhandechetan-code <kadambhandechetan@gmail.com>
2026-07-27 22:12:03 +0530

    Add files via upload

---
+++ string program5.c
@@ -0,0 +1,27 @@
+//WAP to Count the Number of Vowels in a String
+
+#include<stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0, count = 0;
+
+    printf("Enter String : ");
+    scanf("%s", str);
+
+    while(str[i] != '\0')
+    {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' ||
+           str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+        {
+            count++;
+        }
+
+        i++;
+    }
+
+    printf("Number of Vowels = %d", count);
+
+    return 0;
+}
